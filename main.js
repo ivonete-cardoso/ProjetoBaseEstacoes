88361d68eec19e8f7670aa8fad7719fcffe0d13c
ivonete-cardoso <125990682+ivonete-cardoso@users.noreply.github.com>
2023-10-31 00:58:47 +0000

    casa

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 alert("Bem vindo a esta página, eu sou um alert, e você pode me excluir se desejar!")
+const PI = 3,14159;
+document.querySelector("#meuDiv");
+const carro = document.querySelector("fusca");
+
+function tocaSom(som){
+    alert('clap clap clap')
+   
+}
+let comando = "acelerar";
+   if (comando =="acelerar"){
+    alert ("carro está andando");
+
+}else{
+    alert("carro parado")
+}
+const buttons = document.querySelectorAll("button");
+    let contador = 0;
+white (contador < buttons.length){
+    buttons [contador].textContent = contador;
+    contador = contador + 1;
+
+}
